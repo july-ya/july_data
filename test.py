03581975b826e4722e98a3c25f26037a61a1485b
july-ya <123149377+july-ya@users.noreply.github.com>
2023-02-02 16:23:14 +0900

    Update test.py
    [23.02.02]
    1) global aa 생성 및 aa = money 추가
    2) 야구 순위 차트 하단에 표출했던 money 데이터 프레임 주석 처리
    3) 야구 순위 분석한 것 tab 생성하여 chart탭과 data탭으로 분류

--- test.py
+++ test.py
@@ -3,22 +3,38 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+# 제목
 st.title(':rabbit: This is :orange[july-ya] page :rabbit:')
 #st.sidebar.success("Select a demo above.")
 
+# plotting_demo 함수
 def plotting_demo():
   
+  # 파일을 업로드하여 그래프 그릴때 사용
   #uploaded_file = st.file_uploader("Choose a file")
-  
   #money = pd.read_csv(uploaded_file)
+  
+  # git에 올린 파일로 그래프 그림
   money = pd.read_csv("money_data7.csv")
   
+  # select box 생성하여 년도 선택
   option = st.selectbox('How would you like to choice year ?', ('2020', '2021', '2022'))
+  
+  # 선택한 년도를 option2에 할당
   option2 = int(option)
+  
+  # 선택한 년도가 뭐인지 확인 시켜줌
   st.write('You selected:', option)
 
+  # 서낵한 년도에 해당하는 money 데이터 프레임을 money에 할당
   money = money[:] [money['A_YEAR']== option2]
 
+  # 전역변수 aa 설정
+  global aa
+  
+  # money 데이터 프레임을 aa에 할당
+  aa = money
+  
   fig, ax = plt.subplots(2,2, figsize=(12,8))
 
   plt.subplot(221)
@@ -43,8 +59,10 @@ def plotting_demo():
 
   st.snow()
   st.pyplot(fig)
-  st.dataframe(money)
+  
+  #st.dataframe(money)
 
+# bar_chart 함수 생성
 def bar_chart():
   url = "https://sports.news.naver.com/kbaseball/record/index?category=kbo&year="
   years = ['2015', '2016','2017', '2018', '2019', '2020', '2021', '2022' ]
@@ -93,10 +111,15 @@ with st.form(key = 'Form1'):
                                                               '다른 데이터 분석(진행중)'))
 
 if select_language == '금리와 집 값 빠르게 파악하기':
-  try:
+  tab1, tab2 = st.tabs(["📈 Chart", "🗃 Data"])
+  
+  with tab1:
+    tab1.subheader("A tab with a chart")
     plotting_demo()
-  except:
-    pass
-
+    
+  with tab2:
+    tab2.subheader("A tab with the data")
+    st.dataframe(aa)
+    
 elif select_language == '야구 순위와 승률 빠르게 파악하기':
   bar_chart()
